8fa22e1aa283962842a1249ff2d4a43e0f6de327
Gois <matheusgoislimasilva@gmail.com>
2021-06-04 20:48:13 -0300

    Initial Commit

---
+++ pomodoro WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  pomodoro WatchKit Extension
+//
+//  Created by Matheus Silva on 04/06/21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
